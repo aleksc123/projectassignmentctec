1f0d4b25285017a947781e04673b487d552d95b9
Aleks Crnomarkovic <P14135697@LEC-ADMIN.dmu.ac.uk>
2017-01-26 11:08:50 +0000

    Working on web pages and looking at JavaScript

---
+++ javascript/javascripthomepage.js
@@ -0,0 +1,20 @@
+var ThisIndex = 0;
+carousel();
+function carousel(){
+var a;
+var b = document.getElementsByClassName("MainSlideshow");
+for(a=0; a<b.length; a++){
+b[a].style.display="none";
+}
+ThisIndex++;
+if(ThisIndex>b.length){ThisIndex = 1}
+b[ThisIndex-1].style.display = "block";
+setTimeout(carousel, 4000);
+}
+
+function DisplayLinks(){
+	var socialmedia1 = 'LCFC on Facebook';
+	var displaypage = socialmedia1.link("https://en-gb.facebook.com/lcfc/");
+	
+	document.getElementById("display").innerHTML = displaypage;
+}
